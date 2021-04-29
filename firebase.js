f1a96f9223d70831f2725c75fd89bf34a2365195
Jesse <jessedaniel@live.ca>
2021-04-29 15:47:42 -0600

    add firebase app and auth

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCC70THSQwLSOnY3r_rCs0_EADJX7MasVw",
   authDomain: "signal-clone-fb.firebaseapp.com",
@@ -6,3 +10,16 @@ const firebaseConfig = {
   messagingSenderId: "708034129881",
   appId: "1:708034129881:web:ca02fc50724a1192e6b3c8"
 };
+
+let app;
+
+if (firebase.apps.length ===0) {
+app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
