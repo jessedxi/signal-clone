f7ef3a071b06e223e107f26cadb6c8bd48005f08
Jesse <jessedaniel@live.ca>
2021-04-29 12:09:16 -0600

    start firebase config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCC70THSQwLSOnY3r_rCs0_EADJX7MasVw",
+  authDomain: "signal-clone-fb.firebaseapp.com",
+  projectId: "signal-clone-fb",
+  storageBucket: "signal-clone-fb.appspot.com",
+  messagingSenderId: "708034129881",
+  appId: "1:708034129881:web:ca02fc50724a1192e6b3c8"
+};
