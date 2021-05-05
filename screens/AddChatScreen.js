5d8f721b5898e28cfb501e847776fdfef18a720a
Jesse <jessedaniel@live.ca>
2021-05-05 15:52:21 -0600

    add input

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button, Input } from "react-native-elements"
 
 const AddChatScreen = ({ navigation }) => {
+  const [input, setInput] = useState("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Add a new Chat",
+      headerBackTitle: "Chats",
+    })
+
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>Add Chat Screen</Text>
+    <View style={styles.container}>
+      <Input placeholder="Enter a chat name"
+        calue={input}
+        onChangeText={(text) => setInput(text)}
+        leftIcon={
+          <Icon name="wechat" type="antdesign" size={24} color="black" />
+        }
+      />
     </View>
   )
-}
+};
+
 
 export default AddChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+
+  }
+})
