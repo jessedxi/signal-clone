6acd9cc788ce07b894ce956b1a57e83397931913
Jesse <jessedaniel@live.ca>
2021-05-05 10:57:26 -0600

    add avatar to top left

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,23 @@
-
+import { auth } from "../firebase";
 import React, { useLayoutEffect } from 'react'
-import { StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, ScrollView, View } from 'react-native'
 import CustomListItem from './CustomListItem'
+import { Avatar } from "react-native-elements"
 
 
 const HomeScreen = ({ navigation }) => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: ""
+      title: "Signal",
+      headerStyle: { backgroundColor: "#fff"},
+      headerTitleStyle: { color: "black" },
+      headerTintColor: "black",
+      headerLeft: () => (
+         <View style={{ marginLeft: 20 }}>
+        <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }}/>
+      </View>
+      ),
     });
   }, []);
 
