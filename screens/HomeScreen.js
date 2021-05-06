324c6bb24da0f322d6333513a0f9c3b13f3badd6
Jesse <jessedaniel@live.ca>
2021-05-06 10:54:29 -0600

    add use effect to pull chats from db

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,36 @@
-import { auth } from "../firebase";
-import React, { useLayoutEffect } from 'react'
-import { StyleSheet, Text, SafeAreaView, ScrollView, View, TouchableOpacity } from 'react-native'
-import CustomListItem from './CustomListItem'
-import { Avatar } from "react-native-elements"
+import { auth, db } from "../firebase";
+import React, { useLayoutEffect, useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import CustomListItem from "./CustomListItem";
+import { Avatar } from "react-native-elements";
 import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
 
-
 const HomeScreen = ({ navigation }) => {
+  const [chats, setChats] = useState([]);
 
   const signOutUser = () => {
     auth.signOut().then(() => {
       navigation.replace("Login");
-    })
-  }
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = db.collection("chats").onSnapshot((snapshot) =>
+      setChats(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -28,16 +46,21 @@ const HomeScreen = ({ navigation }) => {
         </View>
       ),
       headerRight: () => (
-        <View style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: 80,
-          marginRight: 20,
-        }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 80,
+            marginRight: 20,
+          }}
+        >
           <TouchableOpacity activeOpacity={0.5}>
             <AntDesign name="camerao" size={24} color="black" />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate("AddChat")} activeOpacity={0.5}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("AddChat")}
+            activeOpacity={0.5}
+          >
             <SimpleLineIcons name="pencil" size={24} color="black" />
           </TouchableOpacity>
         </View>
@@ -45,17 +68,15 @@ const HomeScreen = ({ navigation }) => {
     });
   }, []);
 
-
-
   return (
     <SafeAreaView>
       <ScrollView>
         <CustomListItem />
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
